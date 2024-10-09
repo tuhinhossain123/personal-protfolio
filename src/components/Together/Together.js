f625d3206062c63cf3147da0e0137077888c7ed0
tuhinhossain123 <csetuhin55@gmail.com>
2024-10-09 20:40:17 +0600

    together section add

---
+++ src/components/Together/Together.js
@@ -0,0 +1,17 @@
+const Together = () => {
+  return (
+    <div className="mt-36">
+      <div>
+        <h2 className="text-[65px] font-semibold text-center pb-4">
+          Lets Design Together
+        </h2>
+        <p className="text-xl font-normal text-center pb-12">
+          Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh
+          lectus <br /> netus in. Aliquet donec morbi convallis pretium
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Together;
